d41d9d8ae451c94418fe8e841cc937deb5dda840
Ben <ben.c.hawker@gmail.com>
2015-09-11 14:46:01 +0100

    Adding first test, all dependencies installed

---
+++ js/toDoListController.js
@@ -0,0 +1,3 @@
+toDoList.controller("toDoListController", [function() {
+
+}]);
---
+++ test/toDoListController.spec.js
@@ -0,0 +1,16 @@
+describe("toDoListController", function() {
+	beforeEach(module("toDoList"));
+
+		var ctrl;
+
+		beforeEach(inject(function($controller) {
+			ctrl = $controller("toDoListController");
+		}));
+
+	it("starts with an empty entry field and results list", function() {
+		expect(ctrl.entry).toBeUndefined();
+		expect(ctrl.list).toBeUndefined();
+	});
+
+});
+
