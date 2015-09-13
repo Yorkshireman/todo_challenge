ed5a9caeda0425653de599593006770632d7e3aa
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-09-13 17:14:07 +0100

    edit task form is hidden to begin with

--- test/e2e/toDoListFeature.js
+++ test/e2e/toDoListFeature.js
@@ -2,11 +2,19 @@ describe('toDoList', function() {
 
   var taskBox = element(by.model('task'));
   var taskList = element(by.css('#task_list'));
+
   var addTaskButton = element(by.css('#task_submit'));
+
   var deleteTaskButton = element(by.css('#task_delete'));
+
+  var editTaskButton = element(by.css('#task_edit'));
+  var editTaskForm = element(by.css('#edit-task-form'));
+  var editTaskBox = element(by.css('edit-task-text-box'));
 
   beforeEach(function() {
     browser.get('http://localhost:8080');
+    taskBox.sendKeys("First Task");
+    addTaskButton.click();
   });
 
   it('has a title', function() {
@@ -14,17 +22,20 @@ describe('toDoList', function() {
   });
 
   it('displays created tasks', function() {
-    taskBox.sendKeys("First Task");
-    addTaskButton.click();
-
     expect(taskList.getText()).toContain('First Task');
   });
 
   it('can delete a task', function() {
-    taskBox.sendKeys("First Task");
-    addTaskButton.click();
     deleteTaskButton.click();
-
     expect(taskList.getText()).not.toContain('First Task');
   });
+
+  it('before editing a task, the edit task box is hidden', function() {
+    expect(editTaskForm.isDisplayed()).toBe(false);
+  });
+
+  // it('when editing a task, the edit task box is pre-populated', function() {
+  //   editTaskButton.click();
+  //   expect(editTaskBox.getText()).toEqual('First Task');
+  // });
 });
